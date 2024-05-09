4e02e1c832920a4ffaf8f16d3d8b02bde1a522c7
Welton Castoldi <weltoncastoldi@hotmail.com>
2024-05-08 21:57:37 -0300

    remove todos arquivos

--- APPLICATION/MetalCoin.Domain/Enums/TipoFornecedor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MetalCoin.Domain.Enums
-{
-    public enum TipoFornecedor
-    {
-        PessoaFisica = 1,
-        PessoaJuridica = 2
-    }
-}
--- APPLICATION/MetalCoin.Domain/Enums/TipoPerfil.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MetalCoin.Domain.Enums
-{
-    public enum TipoPerfil
-    {
-        Administrador = 1,
-        Usuario = 2
-    }
-}
